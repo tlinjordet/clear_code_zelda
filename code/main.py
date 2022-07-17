72b005583ab000d0b667f77e53eb38f3289e9541
Trond Linjordet <trond.linjordet@uis.no>
2022-07-17 15:45:06 +0200

    Type out initial code, copy world map

---
+++ code/main.py
@@ -0,0 +1,30 @@
+import pygame, sys
+from settings import *
+
+# from debug import debug
+
+
+class Game:
+    def __init__(self):
+
+        # general setup
+        pygame.init()
+        pygame.display.set_caption("Zelda-like")
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            self.screen.fill("black")
+            # debug("hello :-D")
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
